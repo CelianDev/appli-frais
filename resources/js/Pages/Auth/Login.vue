d14001ce76f17391d2d354dfcbc49b5a9f54f3bc
Cel <celian_ck@proton.me>
2025-03-11 00:54:16 +0100

    typo fix jpg to png

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -212,7 +212,7 @@ const submit = () => {
     <div class="relative hidden w-0 flex-1 lg:block">
       <img
         class="absolute inset-0 h-full w-full object-cover"
-        src="/images/login_02.jpg"
+        src="/images/login_02.png"
         alt=""
       />
     </div>
