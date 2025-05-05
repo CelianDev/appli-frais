87bfe296d76baa6865d34115ea76029b631f55dd
Cel <celian_ck@proton.me>
2025-05-05 21:10:38 +0200

    Hiding stats for exam

--- resources/js/Components/Dashboard.vue
+++ resources/js/Components/Dashboard.vue
@@ -4,25 +4,7 @@
     <div class="flex flex-row-reverse gap-4 px-8">
       <main class="flex-1 overflow-visible space-y-4">
         <!-- Main area -->
-        <div id="stats" class="flex flex-col">
-          <div
-            class="flex-1 px-4 py-5 sm:px-6 lg:px-8 lg:py-6 overflow-visible rounded-lg bg-white shadow"
-          >
-            <CardStats />
-          </div>
-        </div>
-        <div class="flex flex-row gap-x-4">
-          <div
-            class="flex-none px-4 py-5 sm:px-6 lg:px-8 lg:py-6 overflow-visible rounded-lg bg-white shadow"
-          >
-            <DoughnutFraisAnnuel />
-          </div>
-          <div
-            class="flex-1 px-4 py-5 sm:px-6 lg:px-8 lg:py-6 overflow-visible rounded-lg bg-white shadow"
-          >
-            <CardListeFicheFrais />
-          </div>
-        </div>
+        
       </main>
       <aside
         class="w-1/4 min-w-96 rounded-lg bg-white shadow overflow-y-auto hidden 2xl:block"
@@ -38,8 +20,4 @@
 
 <script setup>
 import CardFrais from "@/Components/CardFrais.vue";
-import CardStats from "@/Components/CardStats.vue";
-import DoughnutFraisAnnuel from "@/Components/DoughnutFraisAnnuel.vue";
-import StackedBarFraisMonthly from "@/Components/StackedBarFraisMonthly.vue";
-import CardListeFicheFrais from "@/Components/CardListeFicheFrais.vue";
 </script>
